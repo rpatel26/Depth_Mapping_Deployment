631424f73dcc90e95913ab7e49c60860b892069e
EC2 Default User <ec2-user@ip-172-31-24-37.us-west-2.compute.internal>
2019-02-03 20:27:12 +0000

    templated added

---
+++ send_file.py
@@ -0,0 +1,19 @@
+import subprocess, time, os
+
+def send_file():
+	print("sending file to server...")
+	
+        ## Write code between print statement ##
+
+
+	print("file successfully send!")
+
+
+def fetch_result():
+        print("fetching file to the sever...")
+
+        ## write code below the print statement ##
+
+if __name__ == '__main__':
+	send_file()	
+	fetch_result()	
