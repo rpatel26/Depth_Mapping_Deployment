6dce3fa32da6972b0c9cb489469be6c0ebabe897
Ravi Patel <rdp2606@gmail.com>
2019-03-03 16:07:20 -0800

    model.py updated

--- Machine_Learning/model.py
+++ Machine_Learning/model.py
@@ -3,7 +3,7 @@ from keras.models import Sequential
 from keras.layers import Conv2D
 from keras import regularizers
 
-def build_model():
+def build_model(num_classes):
 	model = Sequential()
 	weight_decay = 0.0005
 	x_shape = [32,32,3]	
@@ -11,8 +11,22 @@ def build_model():
 	model.add(Conv2D(64, (3, 3), padding='same',
 	                 input_shape=x_shape,kernel_regularizer=regularizers.l2(weight_decay)))
 
+	# TODO: add an activitaion layer with 'relu' as the activataion function
+	# TODO: add a  batch bormalization layer
+	# TODO: add a dropout layer with 30% dropout rate
+
+	# TODO: add a flatter layer
+	# TODO: add a dense layer with 512 units
+	# TODO: add an activitation layer with 'relu' as the activation function
+	# TODO: add a batch normalization layer
+
+	# TODO: add a dropout layer with 50% dropout rate
+	# TODO: add a dense layer with the units equal to the number of classes
+	# TODO: add a activation layer with 'softmax' as the activation function	
+
 	model.summary()
 
 	return model
 
-model = build_model()
+num_classes = 10
+model = build_model(num_classes)
