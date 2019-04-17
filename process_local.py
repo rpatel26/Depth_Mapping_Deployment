0dfff9c72af7e2453d73dd99d5d6eadaf51322c5
skomatin <skomatin@ucsd.edu>
2019-04-16 23:30:47 -0700

    added process_local.py and geisel.jpg file

---
+++ process_local.py
@@ -0,0 +1,34 @@
+"""
+ECE196 Depth Mapping Project
+Author: Will Chen
+Prerequisite: You need to install OpenCV before running this code
+The code here is an example of what you can write to print out 'Hello World!'
+Now modify this code to process a local image and do the following:
+1. Read geisel.jpg
+2. Convert color to gray scale
+3. Resize to half of its original dimensions
+4. Draw a box at the center the image with size 100x100
+5. Save image with the name, "geisel-bw-rectangle.jpg" to the local directory
+All the above steps should be in one function called process_image()
+"""
+
+# TODO: Import OpenCV
+
+
+# TODO: Edit this function
+def process_image():
+    return
+
+# Just prints 'Hello World! to screen.
+def hello_world():
+    print('Hello World!')
+    return
+
+# TODO: Call process_image function.
+def main():
+    hello_world()
+    return
+
+
+if(__name__ == '__main__'):
+    main()
